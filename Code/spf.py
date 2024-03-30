2a13e4275ff78ff5bd7b37ef6e9a581171fef5e8
fadri14 <fadri1@proton.me>
2024-03-30 11:49:28 +0100

    Vérification de tests de bases

--- Code/spf.py
+++ Code/spf.py
@@ -6,21 +6,6 @@ class MyInterpreter(Interpreter):
     def start(self, tree):
         return self.visit_children(tree)
 
-    def egalite(self, tree):
-        children0 = self.visit_children(tree[0])
-        children1 = self.visit_children(tree[1])
-
-        if not isinstance(children0, booleen) or not isinstance(children0, booleen):
-            return #erreur
-        return children0 == children1
-
-        """
-        tab = []
-        for token in tree.scan_values(lambda x: isinstance(x, Token)):
-            tab += token.value
-        return tab[0] == tab[1]
-        """
-
 parser = Lark.open("spf.lark", parser='lalr')
 interpreter = MyInterpreter()
 
